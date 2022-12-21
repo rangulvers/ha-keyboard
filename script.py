7a2bec32d0e3ff59cc4e1ac214fd2b1f56fd0b90
I511999 <malte.sohns@googlemail.com>
2022-12-21 14:54:42 +0100

    removed debug script. Changed color space to color sys

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ import mido
 import time
 import logging
 import traceback
+import colorsys
 
 server = "http://192.168.2.136:8123"
 endpoint = "api"
@@ -19,22 +20,30 @@ headers = {
 
 
 def convertMidiToRGB(note):
-    if note in range(1, 11):
-        rgb_color = [192, 57, 43]
-    if note in range(11, 20):
-        rgb_color = [155, 89, 182]
-    if note in range(20, 30):
-        rgb_color = [41, 128, 185]
-    if note in range(30, 40):
-        rgb_color = [26, 188, 156]
-    if note in range(40, 50):
-        rgb_color = [39, 174, 96]
-    if note in range(50, 60):
-        rgb_color = [241, 196, 15]
-    if note in range(60, 70):
-        rgb_color = [243, 156, 18]
-    if note in range(70, 90):
-        rgb_color = [211, 84, 0]
+
+    n = 360/88
+    rad = round(n*note)
+
+    r, g, b = colorsys.hls_to_rgb(rad/360, 0.5, 0.5)
+
+    rgb_color = [round(r*255), round(g*255), round(b*255)]
+
+    # if note in range(1, 11):
+    #     rgb_color = [192, 57, 43]
+    # if note in range(11, 20):
+    #     rgb_color = [155, 89, 182]
+    # if note in range(20, 30):
+    #     rgb_color = [41, 128, 185]
+    # if note in range(30, 40):
+    #     rgb_color = [26, 188, 156]
+    # if note in range(40, 50):
+    #     rgb_color = [39, 174, 96]
+    # if note in range(50, 60):
+    #     rgb_color = [241, 196, 15]
+    # if note in range(60, 70):
+    #     rgb_color = [243, 156, 18]
+    # if note in range(70, 90):
+    #     rgb_color = [211, 84, 0]
     return rgb_color
 
 
@@ -78,6 +87,16 @@ def playDemo(light):
     mid = mido.MidiFile('bells.mid')
     brightness_pct = 0
 
+    print("######## RUNNING DEMO MODE #########")
+    for x in range(1, 89):
+        brightness_pct = 100
+        color = convertMidiToRGB(x)
+        print(f"Bright : {brightness_pct} || Note : {x} ==> Color : {color}")
+        changeLightToHA(brightness_pct, color, light)
+        time.sleep(0.2)
+
+    print("########## PLAYING DEMO SONG ##########")
+    time.sleep(1)
     for msg in mid.play():
         if msg.type == "note_on":
             brightness_pct = convertMidiVelocityToRange(msg.velocity)
@@ -100,12 +119,13 @@ def listPorts():
 
 
 def runDebug(midiPort):
-    logging.basicConfig(format='%(levelname)s%:%(message)s', level=logging.DEBUG)
+    logging.basicConfig(format='%(levelname)s%:%(message)s',
+                        level=logging.DEBUG)
     logging.info("####STARTING DEBUG####")
     midiInput = mido.open_input(midiPort)
     for msg in midiInput:
         logging.debug(msg)
-        #if msg.type == "note_on":
+        # if msg.type == "note_on":
         #    brightness_pct = convertMidiVelocityToRange(msg.velocity)
         #    color = convertMidiToRGB(msg.note)
         #    print(f"{msg.velocity} ==> {brightness_pct} | {msg.note} ==> {color}")
